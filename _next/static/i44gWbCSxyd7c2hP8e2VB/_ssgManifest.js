9df4ef2686e899f7d17a3633a941d0fc967ae840
CAPTAIN <30854087+APIDev2021@users.noreply.github.com>
2024-11-15 08:10:50 +0800

    commit

---
+++ _next/static/i44gWbCSxyd7c2hP8e2VB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[locale]","\u002F[locale]\u002Fdistricts","\u002F[locale]\u002Fdistricts\u002F[district_id]","\u002F[locale]\u002Fdistricts\u002F[district_id]\u002F[node_id]","\u002F[locale]\u002Femail-confirmation","\u002Fexperience","\u002Fmap"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
